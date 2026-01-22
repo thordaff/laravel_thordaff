1b5d64bc16d08f6a2e98c7dac6fb72bb54a5217b
Thordaff <thordaf2305@gmail.com>
2026-01-22 15:56:32 +0700

    fix: resolve Bootstrap dropdown functionality issue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 import './bootstrap';
+
+import * as bootstrap from 'bootstrap';
+window.bootstrap = bootstrap;
+
+$(document).ready(function() {
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+});
